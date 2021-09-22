0331b6d6a33a6be0671c59344eb5cac444e269f9
Wellington Guimaraes <wellguimaraes@gmail.com>
2021-09-22 13:14:17 -0300

    Added to-do comment

--- src/generateTypescriptClient.test.ts
+++ src/generateTypescriptClient.test.ts
@@ -3,13 +3,15 @@ import * as ts from 'typescript'
 import { generateTypescriptClient } from '../src/generateTypescriptClient'
 
 describe('generateTypescriptClient', () => {
-  it('should generate client code', async () => {
+  it('should generate working client code', async () => {
     const generatedCode = await generateTypescriptClient({
       output: path.resolve(__dirname, './testClient.ts'),
       endpoint: 'http://localhost:4000/graphql',
     })
 
+    // TODO: create better tests to check functionality of generated code
     const client = eval(ts.transpile(generatedCode))
+
     const books = await client.queries.booksWithOptionalParams({
       title: true,
       author: true
